578325211af767eb531e551bf6aa9bd04f8de04a
Alqama Hasnain <alqama.hasnain@gmail.com>
2024-03-21 19:13:46 +0530

    Coding  Ninjas Problem 27778

---
+++ src/features/books/review.schema.js
@@ -0,0 +1,29 @@
+// make necessary imports here
+
+import mongoose from "mongoose";
+
+// write your code here
+export const reviewSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5,
+    },
+    target: {
+        type: String,
+        enum: ["Author", "Book"],
+        required: true,
+    },
+    targetId: {
+        type: mongoose.Schema.Types.ObjectId, 
+        refPath: "target",
+        required: true
+    }
+});
+
